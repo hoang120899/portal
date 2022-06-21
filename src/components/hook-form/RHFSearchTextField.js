870d97034a02fe2f676c19d17bd1b0da38cfcb3c
thucnguyen.fecth <thucnguyen@fetch.tech>
2022-06-21 15:26:10 +0700

    feat(base-input-form): add base input using react hook form

---
+++ src/components/hook-form/RHFSearchTextField.js
@@ -0,0 +1,33 @@
+// @mui
+import { InputAdornment } from '@mui/material'
+
+import PropTypes from 'prop-types'
+
+// components
+import Iconify from '@/components/Iconify'
+
+import RHFTextField from './RHFTextField'
+
+RHFSearchTextField.propTypes = {
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+}
+
+export default function RHFSearchTextField({ name, placeholder }) {
+  return (
+    <RHFTextField
+      name={name}
+      placeholder={placeholder}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position='start'>
+            <Iconify
+              icon={'eva:search-fill'}
+              sx={{ color: 'text.disabled', width: 20, height: 20 }}
+            />
+          </InputAdornment>
+        ),
+      }}
+    />
+  )
+}
