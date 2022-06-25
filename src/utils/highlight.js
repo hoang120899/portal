8d8f6df673e58aa2e49c882402211c36ce7117d7
thucnguyen.fecth <thucnguyen@fetch.tech>
2022-06-25 12:25:16 +0700

    perf(highlight.js): fix import bundle too size

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -1,9 +1,8 @@
-import hljs from 'highlight.js'
+import hljs from 'highlight.js/lib/core'
+import html from 'highlight.js/lib/languages/vbscript-html'
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
 
-hljs.configure({
-  languages: ['javascript', 'jsx', 'sh', 'bash', 'html', 'scss', 'css', 'json'],
-})
+hljs.registerLanguage('html', html)
 
 if (typeof window !== 'undefined') {
   window.hljs = hljs
