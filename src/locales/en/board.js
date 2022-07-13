e294e6746af6a4d59f12398244869437885bfcec
Dat Bui <datbui@fetch.tech>
2022-07-13 15:07:13 +0700

    feat: add locales for kanban

---
+++ src/locales/en/board.js
@@ -0,0 +1,7 @@
+const board = {
+  moveCard: 'Move Card',
+  storageCard: 'Storage Card',
+  createLabel: 'Create Label',
+}
+
+export default board
---
+++ src/locales/vn/board.js
@@ -0,0 +1,7 @@
+const board = {
+  moveCard: 'Di chuyển thẻ',
+  storageCard: 'lưu thẻ',
+  createLabel: 'Tạo nhãn',
+}
+
+export default board
