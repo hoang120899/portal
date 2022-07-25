22be023b4e003ed6c12fb96fa4875ee608fb385c
Trinh Quang Minh <minh15553@gmail.com>
2022-07-25 15:03:52 +0700

    feat(dashboard/member-activities): display list member last login

--- src/sections/dashboard/member-activities/MemberActivitiesDetails.js
+++ src/sections/dashboard/member-activities/MemberActivitiesDetails.js
@@ -1,37 +1,73 @@
 // @mui
-import { Avatar, Box, Stack, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+
+import { Avatar, Box, Skeleton, Stack, Typography } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 
 import PropTypes from 'prop-types'
 
 import Scrollbar from '@/components/Scrollbar'
+import useLocales from '@/hooks/useLocales'
+import { PATH_DASHBOARD } from '@/routes/paths'
 
 MemberActivitiesDetails.propTypes = {
   list: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool,
 }
 
-export default function MemberActivitiesDetails({ list = [] }) {
+export default function MemberActivitiesDetails({ list = [], isLoading }) {
+  const theme = useTheme()
+  const { translate } = useLocales()
+  const router = useRouter()
+
+  const handleForwardToProfile = (id) => {
+    router.push(PATH_DASHBOARD.profile.view(id))
+  }
+
   return (
     <Scrollbar sx={{ height: { xs: '510px !important', sm: 'auto' } }}>
       <Stack spacing={3} sx={{ p: 3 }}>
-        {list.map(({ avatar, name, email }, index) => (
-          <Stack direction='row' alignItems='center' key={index}>
-            <Avatar src={avatar} sx={{ width: 48, height: 48 }} />
+        {isLoading ? (
+          <>
+            <Skeleton animation='wave' sx={{ height: 48, width: '100%' }} />
+            <Skeleton animation='wave' sx={{ height: 48, width: '100%' }} />
+            <Skeleton animation='wave' sx={{ height: 48, width: '100%' }} />
+            <Skeleton animation='wave' sx={{ height: 48, width: '100%' }} />
+            <Skeleton animation='wave' sx={{ height: 48, width: '100%' }} />
+            <Skeleton animation='wave' sx={{ height: 48, width: '100%' }} />
+          </>
+        ) : (
+          list.map(({ linkAvatar, name, lastLogin, id }, index) => (
+            <Stack direction='row' alignItems='center' key={id || index}>
+              <Avatar src={linkAvatar} sx={{ width: 48, height: 48 }} />
 
-            <Box sx={{ flexGrow: 1, ml: 2, minWidth: 100 }}>
-              <Typography variant='subtitle2' sx={{ mb: 0.5 }} noWrap>
-                {name}
-              </Typography>
+              <Box sx={{ flexGrow: 1, ml: 2, minWidth: 100 }}>
+                <Typography
+                  variant='subtitle1'
+                  sx={{
+                    mb: 0.5,
+                    cursor: 'pointer',
+                    transition: 'all 0.3s',
+                    '&:hover': {
+                      color: `${theme.palette.primary.main}`,
+                    },
+                  }}
+                  onClick={() => handleForwardToProfile(id)}
+                  noWrap
+                >
+                  {name}
+                </Typography>
 
-              <Typography
-                variant='body2'
-                sx={{ color: 'text.secondary' }}
-                noWrap
-              >
-                {email}
-              </Typography>
-            </Box>
-          </Stack>
-        ))}
+                <Typography
+                  variant='body2'
+                  sx={{ mb: 0.5, color: 'text.secondary' }}
+                >
+                  {translate(lastLogin ? `Last login: ${lastLogin}` : '')}
+                </Typography>
+              </Box>
+            </Stack>
+          ))
+        )}
       </Stack>
     </Scrollbar>
   )
--- src/sections/dashboard/member-activities/index.js
+++ src/sections/dashboard/member-activities/index.js
@@ -1,9 +1,16 @@
+import { useEffect, useRef, useState } from 'react'
+
 // @mui
 import { Card, CardHeader } from '@mui/material'
 
 import PropTypes from 'prop-types'
 
+import EmptyContent from '@/components/EmptyContent'
+import useLocales from '@/hooks/useLocales'
+import useRole from '@/hooks/useRole'
+
 import MemberActivitiesDetails from './MemberActivitiesDetails'
+import { useGetAllMemberLastLoginQuery } from './memberActivitiesSlice'
 
 MemberActivities.propTypes = {
   title: PropTypes.string,
@@ -11,18 +18,43 @@ MemberActivities.propTypes = {
 }
 
 export default function MemberActivities({ title, subheader, ...other }) {
-  const list = [...Array(5).keys()].map((value, index) => ({
-    avatar: `https://minimal-assets-api-dev.vercel.app/assets/images/avatars/avatar_${
-      index + 1
-    }.jpg`,
-    name: `Name ${index + 1}`,
-    email: `Email${index}@gmail.com`,
-  }))
+  const { currentRole } = useRole()
+  const { translate } = useLocales()
+  const [listLastLogin, setListLastLogin] = useState([])
+  const headerRef = useRef(null)
+  const {
+    data = {},
+    isLoading,
+    isFetching,
+  } = useGetAllMemberLastLoginQuery({
+    currentRole,
+  })
+
+  const { list: listMembers = [] } = data?.data || {}
+
+  // const listMemberHeight =
+
+  useEffect(() => {
+    setListLastLogin(listMembers)
+  }, [setListLastLogin, listMembers])
 
   return (
     <Card {...other}>
-      <CardHeader title={title} subheader={subheader} />
-      <MemberActivitiesDetails list={list} />
+      <CardHeader title={title} subheader={subheader} ref={headerRef} />
+      {listLastLogin.length > 0 ? (
+        <MemberActivitiesDetails
+          list={listLastLogin}
+          isLoading={isLoading || isFetching}
+        />
+      ) : (
+        <EmptyContent
+          title={translate('No Data')}
+          sx={{
+            height: 'auto',
+            '& span.MuiBox-root': { height: 'auto' },
+          }}
+        />
+      )}
     </Card>
   )
 }
